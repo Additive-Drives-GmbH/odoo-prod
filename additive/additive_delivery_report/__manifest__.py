e55f0a11d1f8d312f92662d225130b86f6f17858
mb-ife <mb@ife.de>
2025-12-18 16:50:32 +0100

    [9254][IMP] additive_delivery_report: adjust delivered and ordered columns font to bold (#7)

--- additive/additive_delivery_report/__manifest__.py
+++ additive/additive_delivery_report/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Additive Delivery Reports",
-    "version": "18.0.1.1.1",
+    "version": "18.0.1.1.2",
     "category": "Reporting",
     "summary": "Extend and Adjust Delivery Note Report",
     "author": "IFE Gesellschaft für Forschung und Entwicklung",
