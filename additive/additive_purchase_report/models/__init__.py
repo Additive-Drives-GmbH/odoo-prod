7f24e46d38136e1e1462554e2a91e38be222abe5
fb-ife <fouzia.jarrari@ife.de>
2026-01-28 11:52:42 +0100

    [FIx][20586]additive_purchase_report (#15)

--- additive/additive_purchase_report/models/__init__.py
+++ additive/additive_purchase_report/models/__init__.py
@@ -1,1 +1,6 @@
 from . import res_partner
+from . import purchase_order_line
+from . import purchase_order
+from . import res_company
+from . import res_config_settings
+from . import ir_actions_report
