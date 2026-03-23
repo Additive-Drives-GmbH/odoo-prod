64bbb6c55ef3ec8622e99ed29f57022b141353dd
lhalb <l.halbauer@web.de>
2026-03-23 17:21:11 +0100

    [REM][task_70514] Remove `report_description` field and related functionality from purchase and invoice reports

--- OCA/account-invoice-reporting/sale_order_line_position/models/sale_order_line.py
+++ OCA/account-invoice-reporting/sale_order_line_position/models/sale_order_line.py
@@ -1,8 +1,11 @@
+# Copyright 2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
 from odoo import api, fields, models
 
 
-class PurchaseOrderLine(models.Model):
-    _inherit = "purchase.order.line"
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
 
     position = fields.Integer(readonly=True, index=True, default=False)
     position_formatted = fields.Char(compute="_compute_position_formatted")
@@ -15,43 +18,42 @@ class PurchaseOrderLine(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         vals_list = self._add_next_position_on_new_line(vals_list)
-        records = super().create(vals_list)
-        return records
+        return super().create(vals_list)
 
     def unlink(self):
-        purchases = self.mapped("order_id")
+        sales = self.mapped("order_id")
         res = super().unlink()
-        for purchase in purchases:
-            purchase.recompute_positions()
+        for sale in sales:
+            sale.recompute_positions()
         return res
 
     def _add_next_position_on_new_line(self, vals_list):
-        purchase_ids = [
+        sale_ids = [
             line["order_id"]
             for line in vals_list
             if not line.get("display_type") and line.get("order_id")
         ]
-        if purchase_ids:
-            ids = tuple(set(purchase_ids))
+        if sale_ids:
+            ids = tuple(set(sale_ids))
             self.flush_model()
             query = """
-            SELECT order_id, coalesce(max(position), 0) FROM purchase_order_line
+            SELECT order_id, coalesce(max(position), 0) FROM sale_order_line
             WHERE order_id in %s GROUP BY order_id;
             """
             self.env.cr.execute(query, (ids,))
-            default_pos = {key: 10 for key in ids}
+            default_pos = {key: 1 for key in ids}
             existing_pos = {
-                order_id: pos + 10 for order_id, pos in self.env.cr.fetchall()
+                order_id: pos + 1 for order_id, pos in self.env.cr.fetchall()
             }
-            purchase_pos = {**default_pos, **existing_pos}
+            sale_pos = {**default_pos, **existing_pos}
             for line in vals_list:
                 if not line.get("display_type"):
-                    line["position"] = purchase_pos[line["order_id"]]
-                    purchase_pos[line["order_id"]] += 10
+                    line["position"] = sale_pos[line["order_id"]]
+                    sale_pos[line["order_id"]] += 1
         return vals_list
 
     @api.model
     def _format_position(self, position):
         if not position:
             return ""
-        return str(position).zfill(4)
+        return str(position).zfill(3)
