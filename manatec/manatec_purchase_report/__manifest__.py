e693470b1867238db5ecbd48979004bc4391d186
lhalb <l.halbauer@web.de>
2026-05-08 16:33:20 +0200

    [ADD][stage] Introduce dummy `manatec_purchase_report` module for deletion purposes on odoo.sh

---
+++ manatec/manatec_purchase_report/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'manaTec Purchase Report (Dummy)',
+    'summary': 'Dummy module for odoo.sh, to be able to delete Module',
+    'description': 'dummy module',
+    'author': "manaTec GmbH",
+    'website': 'https://www.manatec.de',
+    'support': 'info@manatec.de',
+    'category': 'Purchase Management',
+    'version': '18.0.1.0.0',
+    'license': 'OPL-1',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
